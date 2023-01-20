f7f74a9b2fd2fad86b128c2cf161ad4cd26870ac
IlyaLevchenko04 <nneo2086@gmail.com>
2023-01-21 00:39:55 +0200

    r

--- src/js.js
+++ src/js.js
@@ -1,18 +1,10 @@
 
-
 import axios from "axios";
 import Notiflix from "notiflix";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
-import "simplelightbox/dist/simple-lightbox.min.css";
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 
-
-
-const simple = new SimpleLightbox('.gallery .link', { 
-    captionsData: 'alt',
-    captionPosition : 'bottom',
-    captionDelay : 250,
-    nav: true,
-    close: true });
+const loadMore = document.querySelector('.load-more')
 let page = 1;
 const form = document.querySelector('#search-form');
 const input = document.querySelector('.input');
@@ -21,32 +13,35 @@ const gallery = document.querySelector('.gallery');
 const guard = document.querySelector('.js-guard');
 const apiKey = '32923550-e97d894c3a0a0654cb5be36c1'
 const BASE_URL = `https://pixabay.com/api/?key=${apiKey}&`;
+loadMore.setAttribute('hidden', 'true')
 
-form.addEventListener('submit', onSubmit);
+let lightbox = new SimpleLightbox('.gallery a',{ 
+  captionsData: 'alt',
+  captionPosition : 'bottom',
+  captionDelay : 250
+});
+lightbox.refresh();
 
-async function  onSubmit(evt){
-    evt.preventDefault();
+form.addEventListener('submit', onSubmitSearch);
+loadMore.addEventListener('click', onClick)
+
+async function API(page){
     try{
-        const response = await axios.get(`${BASE_URL}q=${input.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`);
-        const data = await response.data;  
-       if(data.hits.length > 0){
-           createMarkup(data.hits);
-          console.log(data)
-       }else{
-        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-       }
+        const response = await axios.get(`${BASE_URL}q=${input.value}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
+        const data = await response.data;
+        console.log(response)
+       return data
     }catch (err){
         console.log(err)
     }
     
 }
 
-
 async function createMarkup(mass){
     
     
     const markup = mass.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-        return `<a href="${largeImageURL}" alt="${tags}" class='link'><div class="photo-card">
+        return `<a href="${largeImageURL}" alt="${tags}" class="link"><div class="photo-card">
         <img src="${webformatURL}" alt="${tags}" loading="lazy" class="card-image"/>
         <div class="info">
           <p class="info-item">
@@ -65,7 +60,66 @@ async function createMarkup(mass){
       </div></a>`
     }).join('');
      gallery.innerHTML += markup;
-     SimpleLightbox.refresh();
+     lightbox.refresh();
 }
 
-console.log('hello')
+async function onClick(evt){
+    evt.preventDefault();
+    page += 1;
+    console.log(page)
+    lightbox.refresh();
+    const data = await API(page);
+    if(page <= Math.ceil(data.total/40)){
+      await createMarkup(data.hits)
+      console.log('if', Math.ceil(data.total/40))
+
+    }else if(page > Math.ceil(data.total/40)){
+        console.log('else if', Math.ceil(data.total/40))
+        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+}
+}
+
+async function onSubmitSearch(evt){
+    try{evt.preventDefault();
+    page = 1;
+    const data = await API(page);
+    lightbox.refresh();
+    if(input.value === ''){
+      throw new Error();
+    }
+    if(data.hits.length > 0){
+      createOneMarkup(data.hits);
+      loadMore.removeAttribute('hidden');
+     console.log(data);
+     return data
+    }else {
+    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    }
+  }catch (err){
+    Notiflix.Notify.failure('Sorry, there are no words in input. Please try again.');
+  }
+}
+
+
+function createOneMarkup(mass){
+  const markup = mass.map(({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
+    return `<a href="${largeImageURL}" alt="${tags}" class='link'><div class="photo-card">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" class="card-image"/>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads ${downloads}</b>
+      </p>
+    </div>
+  </div></a>`
+}).join('');
+ gallery.innerHTML = markup;
+}
